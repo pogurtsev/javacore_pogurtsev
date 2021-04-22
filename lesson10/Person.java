1e56b5755f312a31faaa6f089546cc2c4d33527e
pogurtsev <pogurtsev@yandex.ru>
2021-04-22 10:49:53 +0300

    ДЗ номер 10 коллекции

---
+++ lesson10/Person.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.lesson10;
+
+public class Person {
+    String name;
+    String phone;
+
+    public Person(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
